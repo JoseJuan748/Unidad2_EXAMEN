03be9742a4fa4779a7424f0cd074ad69f37b3388
JoseJuan748 <zap748@amerike.edu.mx>
2023-12-02 02:27:48 -0600

    Revert "4"
    This reverts commit 25f536615fd6fcbffcdef08c78e0503b39e98892.

--- src/MainApp.tsx
+++ src/MainApp.tsx
@@ -1,42 +1,27 @@
 import "./style/Portafolio.css"
 import Inferior from "./components/Inferior"
 
+
 const MainApp = () => {
   return <>
     <h1 className="NameP">Portafolio</h1>
     <div className='Portafolio'>
       <img className='ImgPerfil' src="Yo.jpeg" alt="" />
-      <h2 className='Nombre'>Jose Juan Bañuelos Hernandez
-        <h3 className='Universidad'>Alumno: 3er semestre
-        <h4 className='Datos'>21 años</h4>
-      </h3></h2>
+      <h2 className='Nombre'>Jose Juan Bañuelos Hernandez 
+      </h2>
     </div>
-    <div className='Proyecto1'>
+      
+      <div className='Proyecto1'>
         <img className='ImgProyecto1' src="amerike-top-blanco.png" alt="" />
-    </div>
-
-      <div className="conocimientos">
-        <h2 className="Texto">Conocimiento en legunajes de programacion</h2>
-
-        <h3 className="Name">-C#</h3>
-        <h3 className="Name">-Python</h3>
-        <h3 className="Name">-Typescript</h3>
-        <h3 className="Name">-JavaScrip</h3>
+        <h3 className='TextoProyecto1'>Universidad: Amerike</h3>
+        <h3 className='TextoProyecto1'>Carrera: Desarrollo de software intercativo y videojuegos</h3>
+        <h3 className='TextoProyecto1'>Semestre actual: 3ro</h3>
+        <h3 className='TextoProyecto1'>Conocimientos: C++, html, python</h3>
+        <h3 className='TextoProyecto1'>Redes:</h3>
       </div>
 
-      <div className="Ejemplos">
-        <h3>Animaciones:
-        </h3>
-        <h3>El Cuadrito el cual va a girar en un bucle infinito</h3>
-        <div className="Container">
-          <div className="Cuadrito"></div>
-        </div>
-        <h3>El texto este cambiara su estilo y color</h3>
-        <div className="Container">
-          <div className="Hoverme">Hover me</div>
-        </div>
-        <br /><br />
-      </div>
+      <br /><br /><br /><br /><br />
+
     <Inferior/>
       
   </>
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,28 +1,17 @@
 import MainApp from "../MainApp";
 import Calculadora from "../pages/Calculadora";
-import Encuesta from "../pages/Encuesta";
-import Lista from "../pages/Lista";
-import Contacto from "../pages/Contacto";
-import Numeromagico from "../pages/Numeromagico";
 import Navbar from "./Navbar";
-
-import { Routes, Route } from "react-router-dom";
+import {Routes, Route} from "react-router-dom"
 
 const AppRouter = () => {
-  return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/MainApp" element={<MainApp />} id="MainApp" />
-        <Route path="/Calculadora" element={<Calculadora />} />
-        <Route path="/Encuesta" element={<Encuesta />} /> {}
-        <Route path="/Lista" element={<Lista Name={""} />} /> {}
-        <Route path="/Contacto" element={<Contacto />} /> {}
-        <Route path="/Numeromagico" element={<Numeromagico />} /> {}
-      </Routes>
-    </>
-  );
-};
+  return <>
+        <Navbar/>
+          <Routes >
+              <Route path="/MainApp" element ={<MainApp/>} id="MainApp"/>
+              <Route path="Calculadora" element={<Calculadora/>}/>
+          </Routes>
+  </>
+  
+}
 
-export default AppRouter;
-
+export default AppRouter
